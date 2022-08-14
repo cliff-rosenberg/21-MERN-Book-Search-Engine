c75690c0410692100d840e61385f9d2f37b17e9c
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-13 21:09:27 -0700

    initial setup for Schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,10 @@
+// basic setup for resolvers
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers ={
+
+}
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,8 @@
+// import setup for Apollo Server with Express
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+`;
+
+module.exports = typeDefs;
