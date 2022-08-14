622c21d2995c3b0eedc957ca1b38b5df917056bc
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-08-14 12:02:14 -0700

    updated resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
 // basic setup for resolvers
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
@@ -36,6 +36,28 @@ const resolvers = {
           const token = signToken(user);
     
           return { token, user };
+        },
+        saveBook: async (parent, { newBook }, context) => {
+          if (context.user) {
+            const updatedUser = await User.findByIdAndUpdate(
+              { _id: context.user._id },
+              { $push: { savedBooks: newBook }},
+              { new: true }
+            );
+            return updatedUser;
+          }
+          throw new AuthenticationError('You need to be logged in!');
+        },
+        removeBook: async (parent, { bookId }, context) => {
+          if (context.user) {
+            const updatedUser = await User.findByIdAndUpdate(
+              { _id: context.user._id },
+              { $pull: { savedBooks: { bookId }}},
+              { new: true }
+            );
+            return updatedUser;
+          }
+          throw new AuthenticationError('You need to be logged in!');
         },
     }
 };
